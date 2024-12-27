b703062b648d70bcc0a5cee277e71ba9887a6554
Helmut Lord <kellyhlord@gmail.com>
2024-12-26 19:34:10 -0500

    Fixed buttons w/ power management.

---
+++ firmware/esl_peripheral_sync/src/buttons.c
@@ -0,0 +1,18 @@
+#include <zephyr/kernel.h>
+#include <zephyr/device.h>
+#include <zephyr/logging/log.h>
+
+#if defined(CONFIG_PM_DEVICE)
+#include <zephyr/pm/device.h>
+#endif
+
+LOG_MODULE_REGISTER(buttons, LOG_LEVEL_INF);
+
+static const struct device *const buttons_dev = DEVICE_DT_GET(DT_NODELABEL(buttons));
+
+int buttons_init(void) {
+    int err = pm_device_action_run(buttons_dev, PM_DEVICE_ACTION_RESUME);
+	return err;
+}
+
+SYS_INIT(buttons_init, APPLICATION, 90);
